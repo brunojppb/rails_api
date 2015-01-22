a4de91a90ebe6774d7cfe80af7a0bd0d652e68fa
Bruno Paulino <brunojppb@gmail.com>
2015-01-22 10:28:01 -0500

    Adds show action the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,21 +10,8 @@ Rails.application.routes.draw do
 
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       #list our resources here
+      resources :users, only: [:show]
     end
   end
 
 end
-
-
-# require 'api_constraints'
-
-# MarketPlaceApi::Application.routes.draw do
-
-#   devise_for :users
-# # Api definition
-#   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
-#     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-# # We are going to list our resources here
-#     end
-#   end
-# end
